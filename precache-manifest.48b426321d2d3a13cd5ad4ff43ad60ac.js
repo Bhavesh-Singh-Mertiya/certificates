29c9d2ad1e6119b590f5ded08eb21211489d0338
Bhavesh-Singh-Mertiya <26bhaveshsingh@gmail.com>
2020-09-05 11:53:24 +0530

    Updates

---
+++ precache-manifest.48b426321d2d3a13cd5ad4ff43ad60ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b25c9d31a67c3612a4a",
+    "url": "/certificates/static/css/main.4333d981.chunk.css"
+  },
+  {
+    "revision": "1b25c9d31a67c3612a4a",
+    "url": "/certificates/static/js/main.db3d6c2f.chunk.js"
+  },
+  {
+    "revision": "c3c7e5f5db47a1c0df6d",
+    "url": "/certificates/static/js/runtime~main.b76ce0a6.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/certificates/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "ffcece75dcd01d09fecaf1a3f8a72041",
+    "url": "/certificates/index.html"
+  }
+];
